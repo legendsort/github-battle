a80258501db6a594ea5955a21e0a984e95f93f6b
luckyzacha <zachatatem@gmail.com>
2017-11-06 16:16:09 +0800

    add react-router nav bar

--- app/components/App.js
+++ app/components/App.js
@@ -1,5 +1,10 @@
 const React = require('react')
+const ReactRouter = require('react-router-dom')
+const Router = ReactRouter.BrowserRouter
+const Route = ReactRouter.Route
+
 const Popular = require('./Popular')
+const Nav = require('./Nav')
 
 // Define a React App component here. A component may have:
 // - state
@@ -8,9 +13,12 @@ const Popular = require('./Popular')
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-        <Popular/>
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Route path='/popular' component={Popular} />
+        </div>
+      </Router>
     )
   }
 }
