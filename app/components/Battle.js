78cbd09372064ee915985bc8172ce739911180e4
luckyzacha <zachatatem@gmail.com>
2017-11-06 20:05:58 +0800

    add Battle PlayerInput dummy template

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,10 +1,38 @@
 const React = require('react');
+const PlayerInput = require('./PlayerInput')
 
 class Battle extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      playerOneName: '',
+      playerTwoName: '',
+      playerOneImage: null,
+      playerTwoImage: null,
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(id, username) {
+    this.setState(function () {
+      const newState = {}
+      newState[`${id}Name`] = username
+      newState[`${id}Imnage`] = `https://github.com/${username}/.png?size=200`
+      return newState
+    })
+  }
+
   render() {
+    const playerOneName = this.state.playerOneName
+    const playerTwoName = this.state.playerTwoName
     return (
       <div>
-        Battle!
+        <div className='row'>
+          {!playerOneName && <PlayerInput />}
+          {!playerTwoName && <PlayerInput />}
+        </div>
       </div>
     )
   }
---
+++ app/components/PlayerInput.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class PlayerInput extends React.Component {
+  render() {
+    return (
+      <div>
+        PlayerInput
+      </div>
+    )
+  }
+}
+
+module.exports = PlayerInput
