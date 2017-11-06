fe388a3eaf58d46fc844dbbf447cba9cbebb7c68
luckyzacha <zachatatem@gmail.com>
2017-11-06 16:43:15 +0800

    make refresh at route work with webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,16 @@ const HtmlWebpackPlugin = require('html-webpack-plugin') //installed via npm
 
 
 // For explanation, visit webpack doc at https://webpack.js.org/concepts/
+// Note:
+//   - whenever we hit refresh at a route we get error "cannot get /...". To solve this:
+//     - add config.output.publicPath = '/'
+//     - add config.devServer.historyApiFallback = true
 const config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -16,11 +21,17 @@ const config = {
 
     ]
   },
+  devServer: {
+    historyApiFallback: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './app/index.html',
     })
   ]
 }
+
+
+
 
 module.exports = config;
