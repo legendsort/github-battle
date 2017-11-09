27bb41b3748601480a03545e2f58d302c642b30c
luckyzacha <zachatatem@gmail.com>
2017-11-09 19:16:55 +0800

    refactor to es6 syntax

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -8,67 +8,60 @@ const Loading = require('./Loading')
 
 
 // Stateless Functional Component
-function SelectLanguage(props) {
+function SelectLanguage({onSelect, selectedLanguage}) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
   // console.log(this.props)
   return (
     <ul className='languages'>
-      {languages.map(function(lang) {
-        return (
-          <li
-            key={lang}
-            onClick={props.onSelect.bind(null, lang)}
-            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}>
-            {lang}
-          </li>
-        )
-      }, this)}
+      {languages.map((lang) => (
+        <li
+          key={lang}
+          onClick={onSelect.bind(null, lang)}
+          style={lang === selectedLanguage ? {color: '#d0021b'} : null}>
+          {lang}
+        </li>
+      ))}
     </ul>
   )
 }
-
 SelectLanguage.PropTypes = {
-  selectedLanguage: PropTypes.string.isRequired,
   onSelect: PropTypes.func.isRequired,
+  selectedLanguage: PropTypes.string.isRequired,
 }
 
 
-function ReposGrid (props) {
+function ReposGrid ({repos}) {
   return (
     <ul className='popular-list'>
-      {props.repos.map(function(repo, index) {
-        return (
-          <li key={repo.name} className='popular-item'>
-            <div className='popular-rank'>
-              #{index + 1}
-            </div>
+      {repos.map(({ name, owner, stargazers_count, html_url }, index) => (
+        <li key={name} className='popular-item'>
+          <div className='popular-rank'>
+            #{index + 1}
+          </div>
 
-            <ul className='space-list-items'>
-              <img
-                className='avatar'
-                src={repo.owner.avatar_url}
-                alt={'Avatar for ' + repo.owner.login}
-              />
-              <li>
-                <a href={repo.html_url}>{repo.name}</a>
-              </li>
-              <li>
-                @{repo.owner.login}
-              </li>
-              <li>
-                {repo.stargazers_count} stars
-              </li>
-            </ul>
-          </li>
-
-        )
-      })}
+          <ul className='space-list-items'>
+            <img
+              className='avatar'
+              src={owner.avatar_url}
+              alt={'Avatar for ' + owner.login}
+            />
+            <li>
+              <a href={html_url}>{name}</a>
+            </li>
+            <li>
+              @{owner.login}
+            </li>
+            <li>
+              {stargazers_count} stars
+            </li>
+          </ul>
+        </li>
+      ))}
     </ul>
   )
 }
-
 ReposGrid.PropTypes = {
-
+  repos: PropTypes.array.isRequired,
 }
 
 
@@ -85,39 +78,21 @@ class Popular extends React.Component {
   }
 
   componentDidMount() {
-    // Use this block to make the transition "look" longer to run than it is
-    // setTimeout(function() {
-    //   console.log('sleep starts')
-    //   this.updateLanguage(this.state.selectedLanguage)
-    // }.bind(this), 1000);
-
     this.updateLanguage(this.state.selectedLanguage)
-  }
-
-  componentWillUnmount() {
-
   }
 
   // handle stuff
   updateLanguage(lang) {
-    this.setState(function () {
-      return {
-        selectedLanguage: lang,
-        repos: null,
-      }
-    })
+    this.setState(() =>({
+      selectedLanguage: lang,
+      repos: null,
+    }))
 
     // AJAX
     api.fetchPopularRepos(lang)
-      .then(function(repos) {
-        // console.log(repos)
-        this.setState(function () {
-          return {
-            repos: repos,
-          }
-        })
-      }.bind(this))
-
+      .then((repos) => {
+        this.setState(() => ({repos: repos,}))
+      })
   }
 
   // stateless rendering
@@ -125,16 +100,14 @@ class Popular extends React.Component {
     return (
       <div>
         <SelectLanguage
-          selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}
+          selectedLanguage={this.state.selectedLanguage}
         />
         {/*{JSON.stringify(this.state.repos, null, 2)}*/}
 
-        {!this.state.repos
-          ? <Loading text={"woo saa"} speed={100} />
-          : <ReposGrid repos={this.state.repos}/>}
-
-
+        {this.state.repos
+          ? <ReposGrid repos={this.state.repos}/>
+          : <Loading text={"woo saa"} speed={100} />}
       </div>
     )
   }
